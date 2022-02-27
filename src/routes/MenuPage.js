2f60dd01caafd3564c90810d7f407fe2e70b2865
Nahuel <Nahugc258@gmail.com>
2022-02-27 08:54:30 -0300

    menu page bug

--- src/routes/MenuPage.js
+++ src/routes/MenuPage.js
@@ -1,19 +1,18 @@
 import { useEffect, useState } from "react"
-import { Col, Row } from "react-bootstrap"
 import { useParams } from "react-router-dom"
 import menuService from '../services/menuInfo'
 
 export default function MenuPage() {
   const { id } = useParams()
   const [card, setCard] = useState(null)
-  const [info, setInfo] = useState(null)
+  // const [info, setInfo] = useState(null)
   useEffect(() => {
     const getNutritionLabel = async () => {
       try {
         const getCard = await menuService.getMenuNutritionLabel(id)
-        const getInfo = await menuService.getRecipeInformation(id)
+        // const getInfo = await menuService.getRecipeInformation(id)
         setCard(getCard)
-        setInfo(getInfo)
+        // setInfo(getInfo)
       } catch(e) {
         console.log('Error')
         console.log(e)
@@ -26,20 +25,8 @@ export default function MenuPage() {
 
   return (
     <div>
-      <h1>{info.title}</h1>
-      <Row>
-        <Col>
-        {/* <img src={info.image}/> */}
-        <h4>Aditional Info</h4>
-        <ul>
-          <li>Time: {info.readyInMinutes} minutes</li>
-          <li>Price: ${info.pricePerServing}</li>
-        </ul>
-        </Col>
-        <Col> 
+      <h1>Id: {id}</h1>
           <div className="content" dangerouslySetInnerHTML={{__html: card}}></div>
-        </Col>
-      </Row>
     </div>
   )
 }
